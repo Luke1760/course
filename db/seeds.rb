3c8d8bb8be89805c10da523e45d40f82f6386198
Luke1760 <kjd1760@gmail.com>
2020-10-17 07:44:34 +0000

    add: defalut_user_in_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# add 30 default Curriculums
-30.times do 
+# add defalut user
+User.create!(email: 'admin@gmail.com', password: 'admin@gmail.com', password_confirmation: 'admin@gmail.com')
+
+
+# add 30 fake data
+30.times do
   Curriculum.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
