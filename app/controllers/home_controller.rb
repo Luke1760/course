645851b587f1f0a237068d4fe33f7ab17e4f9d49
Luke1760 <kjd1760@gmail.com>
2020-10-18 05:58:15 +0000

    新增: 省略用戶認證，可以不用登入就可以看到 home_page 資訊

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
   def index
     @curriculums = Curriculum.all.limit(3)
     @latest_curriculums = Curriculum.all.limit(3).order(created_at: :desc)
