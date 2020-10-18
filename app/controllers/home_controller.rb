a5977cf99ebf9cb19a81cd571f45421414895b8b
Luke1760 <kjd1760@gmail.com>
2020-10-18 05:22:55 +0000

    styling: latest_curriculums

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @curriculums = Curriculum.all.limit(3)
+    @latest_curriculums = Curriculum.all.limit(3).order(created_at: :desc)
   end
 end
