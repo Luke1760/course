2eef4faa40c26a578e590cf1d7196dda2d14674a
Luke1760 <kjd1760@gmail.com>
2020-10-26 13:43:33 +0000

    extend: the FriendlyId to Curriculum model

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -4,8 +4,12 @@ class Curriculum < ApplicationRecord
   
   belongs_to :user
   
+  
   def to_s
     title
   end
   has_rich_text :description
+  
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
