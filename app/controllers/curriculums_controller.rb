7f60d5d486958559b6a1a07f31107e8698d7723e
Luke1760 <kjd1760@gmail.com>
2020-10-26 13:45:02 +0000

    add: the FriendlyId to Curriculum controller

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -64,7 +64,7 @@ class CurriculumsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_curriculum
-      @curriculum = Curriculum.find(params[:id])
+      @curriculum = Curriculum.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
