85cee2d2b69101ea99060531a6c957a63865fd7f
Luke1760 <kjd1760@gmail.com>
2020-10-26 14:26:59 +0000

    generate: random number and use it to have the slug

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -12,4 +12,15 @@ class Curriculum < ApplicationRecord
   
   extend FriendlyId
   friendly_id :title, use: :slugged
+  
+   friendly_id :generated_slug, use: :slugged
+   def generated_slug
+     require 'securerandom' 
+     @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4) 
+   end
+  
+  def to_s
+    slug
+  end
 end
+
